d23f2a9612a6f077f4ebf4bcce0249b336ee5539
sergiocruz120 <122457897+sergiocruz120@users.noreply.github.com>
2025-01-24 17:53:01 -0300

    fix: Resolve compatibility issues with Bootstrap version

--- lista/src/main.js
+++ lista/src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-import './plugins/bootstrap-vue'
 import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 createApp(App).use(router).use(store).mount('#app')
